1a442743836e7dfb335713e82aa94c8bb9329a7c
daniel <danielgoncalo2002@gmail.com>
2024-06-25 08:32:38 +0000

    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

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,16 @@
 from locust import HttpUser, between, task
 
 class WebsiteUser(HttpUser):
-    wait_time = between(2,5)
+    wait_time = between(1,2)
     
     # @task
     # def index(self):
     #     self.client.get("/regular/search?song=a")
 
-    @task
-    def index2(self):
-        self.client.get("/regular/top-charts?date=2017-01-01&country=Argentina")
-    
     # @task
-    # def index3(self):
-    #     self.client.get("/regular/song-details/1")
+    # def index2(self):
+    #     self.client.get("/regular/top-charts?date=2017-01-01&country=Argentina")
+    
+    @task
+    def index3(self):
+        self.client.get("/regular/song-details/1")
